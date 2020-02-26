13400d6d22347192a3a643a7e365b78689928491
zuri <cohenzuri@gmail.com>
2020-02-26 20:18:05 +0200

    Initial Commit

---
+++ MySideProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySideProject
+//
+//  Created by zuri cohen on 2/26/20.
+//  Copyright © 2020 zuri cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
