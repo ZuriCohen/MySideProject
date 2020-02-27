9e10ad38a6e0ed840396716eef7e68f5854c9103
zuri <cohenzuri@gmail.com>
2020-02-27 08:21:43 +0200

    Start ' device list  '  screen  table view

---
+++ MySideProject/DeviceList/DeviceListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DeviceListViewController.swift
+//  MySideProject
+//
+//  Created by zuri cohen on 2/26/20.
+//  Copyright © 2020 zuri cohen. All rights reserved.
+//
+
+import UIKit
+
+class DeviceListViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupUI()
+        
+    }
+    
+    func setupUI() {
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+    }
+
+ 
+}
+
+extension DeviceListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        return cell
+        
+    }
+  
+}
---
+++ MySideProject/DeviceList/DeviceListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceListViewModel.swift
+//  MySideProject
+//
+//  Created by zuri cohen on 2/27/20.
+//  Copyright © 2020 zuri cohen. All rights reserved.
+//
+
+import Foundation
